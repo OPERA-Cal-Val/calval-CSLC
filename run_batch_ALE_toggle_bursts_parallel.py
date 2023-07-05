ff761bb3e7ca51f6237c9472fd67d53b3929a1b2
gracebato <m.grace.bato@gmail.com>
2023-07-05 14:58:07 -0700

    Added input parser function.

--- run_batch_ALE_toggle_bursts_parallel.py
+++ run_batch_ALE_toggle_bursts_parallel.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+import argparse
 import os
 import warnings
 from pathlib import Path
@@ -9,6 +12,22 @@ import concurrent.futures
 import timeit
 warnings.filterwarnings('ignore')
 
+def createParser(iargs = None):
+    '''Commandline input parser'''
+    parser = argparse.ArgumentParser(description='Absolute Location Error estimation to validate OPERA CSLCs')
+    parser.add_argument("--savedir", dest='savedir',
+                         required=True, type=str, help='Save directory')
+    parser.add_argument("--burst_ids", dest="burst_ids",
+                         required=True, nargs='+', help="List of burst_ids to process, ['t064_135523_iw2', 't071_151224_iw2'] ")
+    parser.add_argument("--snr", dest="snr",
+                         default=15, type=int, help='Signal-to-noise ratio threshold to remove corner reflectors outliers (default:15)')    
+    parser.add_argument("--solidtide", dest="solidtide",
+                         default='False', type=str, help='Correct for solid earth tides (True if yes, False if no)')
+    parser.add_argument("--ovsFactor", dest="ovsFactor",
+                         default=128, type=int, help='Oversampling factor size to locate the peak amplitude (default: 128)')
+    parser.add_argument("--cpuworkers", dest="cpuworkers",
+                         default=10, type=int, help='Number of CPUs to use (default: 10)')
+    return parser.parse_args(args=iargs)
 
 def run_papermill(p):
     # Set Parameters
@@ -38,16 +57,15 @@ def run_papermill(p):
     
     return (f'Finished processing AO ({cr_network}) burst ({burst_id}), for date ({cslc_date})')
 
-def main():
+def main(inps):
     # Specify valid burst(s)
     # Default is to loop through all
-    sample_bursts = ['t064_135523_iw2', 't071_151224_iw2']
-    savedir = '/Users/bato/work/OPERA/CSLC/CalVal/TESTPARALLEL'
-
-    # Burst parameters
-    snr_threshold = 15
-    solidtide = 'True'
-    ovsFactor = 128
+    sample_bursts = inps.burst_ids
+    savedir = inps.savedir
+    snr_threshold = inps.snr
+    solidtide = inps.solidtide
+    ovsFactor = inps.ovsFactor
+    cpuworkers = inps.cpuworkers
 
     # read list of bursts used for validation
     validation_bursts = Path('validation_data/validation_bursts.csv')
@@ -102,7 +120,7 @@ def main():
         # print(list(map(testparallel,params)))
         
         print(f'Number of CPUs your computer have: {os.cpu_count()}')
-        cpuworkers = int(os.cpu_count()/4)
+        # cpuworkers = int(os.cpu_count()/4)
         print(f'Using {cpuworkers} CPUs for this processing.')
         with concurrent.futures.ProcessPoolExecutor(max_workers=cpuworkers) as executor:
             for result in executor.map(run_papermill,params):
@@ -115,4 +133,10 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # load arguments from command line
+    inps = createParser()
+
+    print("Running ALE now")
+   
+    # Run the main
+    main(inps)
