2d821f8a4293c7f5dc978834e0eff9b9a747a1de
Jinwoo Kim (jinwook@smu.edu) <85753525+smuinsar@users.noreply.github.com>
2023-07-10 18:07:54 -0500

    Adding pass/fail criteria to figures

--- run4_RLE_plot_ts_offsets.py
+++ run4_RLE_plot_ts_offsets.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import pandas as pd
+import numpy as np
 import datetime as dt
 import matplotlib.pyplot as plt
 
@@ -14,25 +15,42 @@ def createParser(iargs = None):
     
     return parser.parse_args(args=iargs)
 
+def if_pass(ts,requirement):
+    bool_pass = (ts > -requirement) & (ts < requirement)
+    pass_rate = np.count_nonzero(bool_pass)/len(bool_pass)
+    return bool_pass, pass_rate, pass_rate>0.8
+
 def run(inps):
 
     df = pd.read_csv(inps.csv) 
     df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
 
-    fig, ax = plt.subplots(2,1,figsize=(15,10),sharex=True)
+    rg_bool_pass, rg_pass_rate, rg_pass_or_not = if_pass(df['rg_avg'],0.5)
+    az_bool_pass, az_pass_rate, az_pass_or_not = if_pass(df['az_avg'],0.75)
 
+    fig, ax = plt.subplots(2,1,figsize=(15,10),sharex=True)
     ax[0].set_title('RLE in Ground Range (m)')
     ax[0].axhspan(-0.5,0.5,color='red', alpha=0.05)    #OPERA requirements in ground range
-    ax[0].errorbar(df['date'],df['rg_avg'],df['rg_std'],marker='o',linestyle=' ',ecolor='lightgray', elinewidth=3, capsize=0, zorder=0)
+    ax[0].errorbar(df['date'][rg_bool_pass],df['rg_avg'][rg_bool_pass],df['rg_std'][rg_bool_pass],marker='o',color='b',linestyle=' ',ecolor='lightgray', elinewidth=3, capsize=0, zorder=0)
+    ax[0].errorbar(df['date'][~rg_bool_pass],df['rg_avg'][~rg_bool_pass],df['rg_std'][~rg_bool_pass],marker='o',color='r',linestyle=' ',ecolor='lightgray', elinewidth=3, capsize=0, zorder=0)
     ax[0].set_ylim(-5,5)
     ax[0].grid(axis='x',linestyle='--')
-
+    if rg_pass_or_not:
+        ax[0].text(0.9,0.9,'Pass',color='w',size=15,weight='bold',transform = ax[0].transAxes,bbox=dict(facecolor='blue',boxstyle='round',edgecolor='none'))
+    else:
+        ax[0].text(0.9,0.9,'Fail',color='w',size=15,weight='bold',transform = ax[0].transAxes,bbox=dict(facecolor='red',boxstyle='round',edgecolor='none'))
+    
     ax[1].set_title('RLE in Azimuth (m)')
     ax[1].axhspan(-0.75,0.75,color='red', alpha=0.05)    #OPERA requirements in azimuth
-    ax[1].errorbar(df['date'],df['az_avg'],df['az_std'],marker='o',linestyle=' ',ecolor='lightgray', elinewidth=3, capsize=0, zorder=0)
+    ax[1].errorbar(df['date'][az_bool_pass],df['az_avg'][az_bool_pass],df['az_std'][az_bool_pass],marker='o',color='b',linestyle=' ',ecolor='lightgray', elinewidth=3, capsize=0, zorder=0)
+    ax[1].errorbar(df['date'][~az_bool_pass],df['az_avg'][~az_bool_pass],df['az_std'][~az_bool_pass],marker='o',color='r',linestyle=' ',ecolor='lightgray', elinewidth=3, capsize=0, zorder=0)
     ax[1].set_xlabel('dates')
     ax[1].set_ylim(-5,5)
     ax[1].grid(axis='x',linestyle='--')
+    if az_pass_or_not:
+        ax[1].text(0.9,0.9,'Pass',color='w',size=15,weight='bold',transform = ax[1].transAxes,bbox=dict(facecolor='blue',boxstyle='round',edgecolor='none'))
+    else:
+        ax[1].text(0.9,0.9,'Fail',color='w',size=15,weight='bold',transform = ax[1].transAxes,bbox=dict(facecolor='red',boxstyle='round',edgecolor='none'))
     fig.savefig(inps.png,dpi=300,bbox_inches='tight')
 
 if __name__ == '__main__':
