06905476186f9ee42bcaf1a1f9df67e05164ade3
gracebato <m.grace.bato@gmail.com>
2024-04-22 14:59:06 -0700

    Clean up repo. Removed unnecessary batch runs.

--- run4_RLE_batch.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-# python run4_RLE_plot_ts_offsets.py --savedir /u/trappist-r0/bato/work/cslc_validation/calval_v02/RLE --burst_id "t064_135518_iw1" --refDate 20230701
-python run4_RLE_plot_ts_offsets.py --savedir /u/trappist-r0/bato/work/cslc_validation/calval_v02/RLE --burst_id "t064_135523_iw1" --refDate 20230701
-python run4_RLE_plot_ts_offsets.py --savedir /u/trappist-r0/bato/work/cslc_validation/calval_v02/RLE --burst_id "t064_135523_iw2" --refDate 20230701
-python run4_RLE_plot_ts_offsets.py --savedir /u/trappist-r0/bato/work/cslc_validation/calval_v02/RLE --burst_id "t071_151224_iw2" --refDate 20230701
-python run4_RLE_plot_ts_offsets.py --savedir /u/trappist-r0/bato/work/cslc_validation/calval_v02/RLE --burst_id "t071_151230_iw2" --refDate 20230701
